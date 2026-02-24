cf539cfa01951fc2402c5278205b2c61031d8262
Michael Xu <michael.x@wustl.edu>
2026-02-24 17:12:56 -0600

    about section update

--- personal_website/src/pages/About.jsx
+++ personal_website/src/pages/About.jsx
@@ -12,8 +12,7 @@ function About() {
       </header>
       <section className="mb-10">
         <p className="text-slate-400 leading-relaxed">
-          I'm currently a student at Washington University in St. Louis studying Computer Science. 
-          I'm born and raised in a humble town called Swampscott, Massachusetts and currently live in St. Louis, Missouri.
+          Hello! My name is Michael, and I'm currently a junior at Washington University in St. Louis, pursuing a degree in Computer Science with minors in Human-Computer Interaction and Electrical Engineering. I enjoy working on scalable backend systems as well as hands-on projects in game development and web applications. I am particularly interested in AI and how it is being used both in the workforce and in daily life, from optimizing business processes to enhancing the way people interact with technology. I'm excited to continue exploring opportunities where I can apply these interests to create meaningful and impactful solutions.
         </p>
       </section>
     </div>
